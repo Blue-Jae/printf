89d7f25087a8bba18c85e51713e8e753da8d7057
Idriss Ibrahim <edrissebraheem@gmail.com>
2022-08-09 07:01:56 -0700

    Minor fix

--- _putchar.c
+++ _putchar.c
@@ -2,7 +2,7 @@
 /**
   * _putchar - Function to print a character to the screen
   * @c: Variable to accept the character to be printed
-  * Return: returns a character.
+  * Return : returns a character.
   */
 char _putchar(char c)
 {
--- ji_printchar.c
+++ ji_printchar.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * ji_printchar - Function that prints a character
- * @args: Argument of va_list type
- *
- * Return: Always 1
- */
+  * ji_printchar - Function that prints a character
+  * @args : Argument of va_list type
+  *
+  * Return: Always 1
+  */
 
 int ji_printchar(va_list args)
 {
